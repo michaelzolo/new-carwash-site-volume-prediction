f082016a8a1dddca8a222876b2ecc569f7e69e74
michaelzolo <michael.zolotarevsky@omnixlabs.com>
2022-03-07 18:04:25 +0200

    added parameter function

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world(): 
     return 'Hello World!'
+
+
+@app.route('/<location_id>')
+def hello_world(location_id):
+    return 'Hello World! location: '+location_id
 
 
 if __name__ == '__main__':
